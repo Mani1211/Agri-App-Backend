62d4a4c39d46114acca45e20af19ec872cf489c5
Mani1211 <smanivasagamtnj@gmail.com>
2020-04-03 19:08:14 +0530

    sugar income updated

--- models/sugarincome.js
+++ models/sugarincome.js
@@ -1,41 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 let SugarIncome = new Schema({
-    date: {
-        type: String
-    },
-    customerName: {
-        type: String
-    },
-    customerPlace: {
-        type: String
-    },
-    ryotNumber: {
-        type: Number
-    },
-    plotNumber: {
-        type: Number
-    },
-    costPerTon: {
-        type: Number
-    },
-    totalTons: {
-        type: Number
-    },
-    amount: {
-        type: Number
-    },
-    advance: {
-        type: Number
-    },
-    balance: {
-        type: Number
-    },
-    amountGiven: {
-        type: Number
-    },
-})
+  date: {
+    type: String
+  },
+  customerName: {
+    type: String
+  },
+  customerPlace: {
+    type: String
+  },
 
-module.exports= mongoose.model('SugarIncome', SugarIncome)
+  vehicleNumber: {
+    type: String
+  },
+  ryotNumber: {
+    type: Number
+  },
+  plotNumber: {
+    type: Number
+  },
+  costPerTon: {
+    type: Number
+  },
+  totalTons: {
+    type: Number
+  },
+  amount: {
+    type: Number
+  },
+  advance: {
+    type: Number
+  },
+  balance: {
+    type: Number
+  },
+  amountGiven: {
+    type: Number
+  }
+});
+
+module.exports = mongoose.model("SugarIncome", SugarIncome);
